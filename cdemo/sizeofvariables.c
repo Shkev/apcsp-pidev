70830b28cc7a464f18cb7629ecde35cc2e4535b2
shkev <shkev.az@gmail.com>
2019-05-22 09:08:59 -0700

    adding sizeofvariables.c

---
+++ cdemo/sizeofvariables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+  int a = 999;
+
+  // print value and size of an int variable
+  printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
+
+  unsigned int b = 999;
+  printf("unsigned a value: %d and size: %d bytes\n", b, sizeof(b));
+
+  
+}
+
