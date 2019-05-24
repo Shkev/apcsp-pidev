278b4dc76871f58883565018a0778589bec3a16b
shkev <shkev.az@gmail.com>
2019-05-24 09:06:59 -0700

    changing arrays.c

--- cdemo/arrays.c
+++ cdemo/arrays.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+
+void arrayAdd(int* arr, int s, int n) {
+  for(int i = 0; i < s; i++) {
+    arr[i] = (arr[i] + n);
+    printf("%d\n", arr[i]);
+  }
+}
+
 int main() {
   int arr[100];
   for(int i = 0; i < 100; i++) {
     arr[i] = i * i;
-    printf("%d\n", arr[i]);
   }
+  arrayAdd(arr, 100, 25);
 }
