61a208e99d5e12e9de07c289ba9dfc4b112694ea
woochie <woocheolkim0@gmail.com>
2019-06-03 16:27:52 +0000

    adding changes for Wooch

--- proj/Wooch/linSolveWooch.c
+++ proj/Wooch/linSolveWooch.c
@@ -1,1 +1,15 @@
-//wooch define your functions here
+#include "linSolveWooch.h"
+
+void multMatrix()
+{
+	int a,b;
+	for(a=0;a< row;a++)
+    {
+        for(b=0;b< col;b++)
+        {
+            printf("Enter element [%d,%d] : ",a+1,b+1);
+            scanf("%d",&m[a][b]);
+        }
+    }
+}
+
