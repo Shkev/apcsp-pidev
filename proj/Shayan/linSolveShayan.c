92687b7f3c88589e2767acbce0191cf080bbc8ba
shkev <shkev.az@gmail.com>
2019-06-04 13:23:04 -0700

    changing linSolveShayan.c and linSolveMain.c

--- proj/Shayan/linSolveShayan.c
+++ proj/Shayan/linSolveShayan.c
@@ -7,66 +7,81 @@
 #define TRUE   1
 
 //defining functions
-void rmExtra(int* matrix[MAXROW][MAXCOL], int* r1, int* r2, int* r3, int* r4, int* c1, int* c2, int* c3, int* c4)
+void rmExtra(float* matrix[MAXROW][MAXCOL], int* r1, int* r2, int* r3, int* r4, int* c1, int* c2, int* c3, int* c4)
 {
   int m = 0;
   int n = 0;
-  &r1 = TRUE; &r2 = TRUE; &r3 = TRUE; &r4 = TRUE;
-  &c1 = TRUE; &c2 = TRUE; &c3 = TRUE; &c4 = TRUE;
+  *r1 = TRUE; *r2 = TRUE; *r3 = TRUE; *r4 = TRUE;
+  *c1 = TRUE; *c2 = TRUE; *c3 = TRUE; *c4 = TRUE;
   for (int i = 0; i < MAXROW; i++)
   {
-    if ((matrix[i][n] == 0) & (matrix[i][n + 1] == 0) & (matrix[i][n + 2] == 0) & (matrix[i][n + 3] == 0)
+    if ((matrix[i][n] == 0) & (matrix[i][n + 1] == 0) & (matrix[i][n + 2] == 0) & (matrix[i][n + 3] == 0))
     {
       if (i == 0)
       {
-        &r1 = FALSE;
+        *r1 = FALSE;
       }
       if (i == 1)
       {
-        &r2 = FALSE;
+        *r2 = FALSE;
       }
       if (i == 2)
       {
-        &r3 = FALSE;
+        *r3 = FALSE;
       }
       if (i == 3)
       {
-        &r4 = FALSE;
+        *r4 = FALSE;
       }
     }
+  }
   for (int i = 0; i < MAXCOL; i++)
   {
-    if ((matrix[m][i] == 0) & (matrix[m + 1][i] == 0) & (matrix[m + 2][i] == 0) & (matrix[m + 3][i] == 0)
+    if ((matrix[m][i] == 0) & (matrix[m + 1][i] == 0) & (matrix[m + 2][i] == 0) & (matrix[m + 3][i] == 0))
     {
       if (i == 0)
       {
-        &c1 = FALSE;
+        *c1 = FALSE;
       }
       if (i == 1)
       {
-        &c2 = FALSE;
+        *c2 = FALSE;
       }
       if (i == 2)
       {
-        &c3 = FALSE;
+        *c3 = FALSE;
       }
       if (i == 3)
       {
-        &c4 = FALSE;
+	*c4 = FALSE;
       }
     }
   }
 }
 
-void invMatrix(int* matrix[MAXROW][MAXCOL],int* r1, int* r2, int* r3, int* r4, int* c1, int* c2, int* c3, int* c4 )
+void invMatrix(float matrix[MAXROW][MAXCOL],int* r1, int* r2, int* r3, int* r4, int* c1, int* c2, int* c3, int* c4 )
 {
   float deter;
-  int adj[MAXROW][MAXCOL];
-  if ((&c4 == FALSE) & (&c3 == FALSE) & (&r4 == FALSE) & (&r3 == FALSE))
+  float adj[MAXROW][MAXCOL];
+  float invMat[MAXROW][MAXCOL];
+  if ((*c4 == FALSE) & (*c3 == FALSE) & (*r4 == FALSE) & (*r3 == FALSE))
   {
-    deter = ((matrix[1][1])(matrix[2][2])) - ((matrix[1][2])(matrix[2][1]));
-    adj[1][1] = matrix [2][2];
-    adj[1][2] = (matrix[1][2]) * -1;
+    float a = matrix[1][1];
+    float b = matrix[1][2];
+    float c = matrix[2][1];
+    float d = matrix[2][2];
+    deter = (a * d) -  (b * c);
+    adj[1][1] = d;
+    adj[1][2] = b * (-1);
+    adj[2][1] = c * (-1);
+    adj[2][2] = a;
+    for (int m = 0; m < MAXROW; m++)
+    {
+      for (int n = 0; n < MAXCOL; n++)
+      {
+        invMat[m][n] = adj[m][n] * (1 / deter);	
+      }
+    }  
   }
 }
     
--- proj/linSolveMain.c
+++ proj/linSolveMain.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "linSolveWooch.h"
-#include "linSolveShayan.h"
+//#include "linSolveWooch.h"
+//#include "linSolveShayan.h"
 #define MAXROW 4
 #define MAXCOL 4
 
 /* Start of main code */
-int main(int argc, int* argv[])
+int main(int argc, char* argv[])
 {
-  int inputMatrix[MAXROW][MAXCOL];
-  for (int i = 0, int n = 0, int m = 0; i < sizeof(argv); i++, n++)
+  float inputMatrix[MAXROW][MAXCOL];
+  int n = 0;
+  int m = 0;
+  for (int i = 0; argc; i++)
   {
     if ((n % MAXCOL) == 0)
     {
-      n = 0
-      m++
+      n = 0;
+      m++;
     }
-    inputMatrix[m][n] = argv[i];
+    inputMatrix[m][n] = atof(argv[i]);
+    n++;
   }
+  printf("%f\n", inputMatrix[1][1]);
+  exit(EXIT_SUCCESS);
 }
