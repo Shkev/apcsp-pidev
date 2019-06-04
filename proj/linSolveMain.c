022da58953b2726764d47bb8b80a9114dc934e7a
shkev <shkev.az@gmail.com>
2019-06-04 09:31:36 -0700

    changing linSolveMain.c

--- proj/linSolveMain.c
+++ proj/linSolveMain.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "linSolveWooch.h"
+#include "linSolveShayan.h"
+#define MAXROW 4
+#define MAXCOL 4
+
+/* Start of main code */
+int main(int argc, int* argv[])
+{
+  int inputMatrix[MAXROW][MAXCOL];
+  for (int i = 0, int n = 0, int m = 0; i < sizeof(argv); i++, n++)
+  {
+    if ((n % MAXCOL) == 0)
+    {
+      n = 0
+      m++
+    }
+    inputMatrix[m][n] = argv[i];
+  }
+}
