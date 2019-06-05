9fb17de426bedb81a05cfc1d8c20847721bcf898
shkev <shkev.az@gmail.com>
2019-06-05 09:33:36 -0700

    changing linSolveMain.c

--- proj/linSolveMain.c
+++ proj/linSolveMain.c
@@ -8,10 +8,11 @@
 /* Start of main code */
 int main(int argc, char* argv[])
 {
+  printf("%d", argc);
   float inputMatrix[MAXROW][MAXCOL];
   int n = 0;
   int m = 0;
-  for (int i = 0; argc; i++)
+  for (int i = 0; i < sizeof(*argv); i++)
   {
     if ((n % MAXCOL) == 0)
     {
@@ -21,6 +22,6 @@ int main(int argc, char* argv[])
     inputMatrix[m][n] = atof(argv[i]);
     n++;
   }
-  printf("%f\n", inputMatrix[1][1]);
+  //  printf("%f\n", inputMatrix[1][1]);
   exit(EXIT_SUCCESS);
 }
