2b23f011d67df4249a1526144c06978dd55e3044
shkev <shkev.az@gmail.com>
2019-06-06 08:40:01 -0700

    changing linSolve.php to have spaces between equal sign

--- proj/linSolve.php
+++ proj/linSolve.php
@@ -9,7 +9,7 @@
       }
     </style>
   </head>
-  <body>
+0;256;0c  <body>
 
     <h1>Enter Coefficients of Variables to solve Linear System</h1>
 
@@ -27,7 +27,10 @@
          $arg7 = test_input($_POST["arg7"]);
          $arg8 = test_input($_POST["arg8"]);
          $arg9 = test_input($_POST["arg9"]);
-       exec("/usr/lib/cgi-bin/sp1b/linSolve " . $arg1 . " " . $arg2 . " " . $arg3 . " " . $arg4 . " " . $arg5 . " " . $arg6 . " " . $arg7 . " " . $arg7 . " " . $arg8 . " " . $arg9, $output, $retc);
+         $arg10 = test_input($_POST["arg10"]);
+         $arg11 = test_input($_POST["arg11"]);
+         $arg12 = test_input($_POST["arg12"]);
+       exec("/usr/lib/cgi-bin/sp1b/linSolve " . $arg1 . " " . $arg2 . " " . $arg3 . " " . $arg4 . " " . $arg5 . " " . $arg6 . " " . $arg7 . " " . $arg7 . " " . $arg8 . " " . $arg9 . " " . $arg10 . " " . $arg11 . " " . $arg12, $output, $retc);
        }
 
        function test_input($data) {
@@ -40,9 +43,9 @@
 
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
       
-      Equation 1: <input type="text" name="arg1" value="0"><i>x<sub>1</sub></i> <input type="text" name="arg2" value="0"><i>x<sub>2</sub></i> <input type="text" name="arg3" value="0"><i>x<sub>3</sub></i>
-      <p> </p>Equation 2: <input type="text" name="arg4" value="0"><i>x<sub>1</sub></i> <input type="text" name="arg5" value="0"><i>x<sub>2</sub></i> <input type="text" name="arg6" value="0"><i>x<sub>3</sub></i>
-      <p> </p>Equation 3: <input type="text" name="arg7" value="0"><i>x<sub>1</sub></i> <input type="text" name="arg8" value="0"><i>x<sub>2</sub></i> <input type="text" name="arg9" value="0"><i>x<sub>3</sub></i>
+      Equation 1: <input type="text" name="arg1" value="0"><i>x<sub>1</sub></i> <input type="text" name="arg2" value="0"><i>x<sub>2</sub></i> <input type="text" name="arg3" value="0"><i>x<sub>3</sub></i>&nbsp  =&nbsp  <input type="text" name="arg4" value="0">
+      <p> </p>Equation 2: <input type="text" name="arg5" value="0"><i>x<sub>1</sub></i> <input type="text" name="arg6" value="0"><i>x<sub>2</sub></i> <input type="text" name="arg7" value="0"><i>x<sub>3</sub></i>&nbsp=  &nbsp<input type="text" name="arg8" value="0">
+      <p> </p>Equation 3: <input type="text" name="arg9" value="0"><i>x<sub>1</sub></i> <input type="text" name="arg10" value="0"><i>x<sub>2</sub></i> <input type="text" name="arg11" Value="0"><I>X<sub>3</sub></i>&nbsp  =&nbsp  <input type="text" name="arg12" value="0">
       <br>
       <input type="submit" value="Go!">
     </form>
